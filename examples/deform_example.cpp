f4c095bdc390a64e951dba8e164cd6e88618e17a
Christoph Heindl <christoph.heindl@gmail.com>
2016-05-02 15:53:06 +0200

    Adding sphere test object.

--- examples/deform_example.cpp
+++ examples/deform_example.cpp
@@ -26,10 +26,10 @@ int main(int argc, char **argv) {
         return -1;
     }
     
-    const int nids = 4;
-    const int anchorIds[] = { 3,7,11,15 };
+    const int nids = 1;
 
     //const int anchorIds[] = { 0,1,2,3,40,41,60,61,80,81,100,101,120,121,140,141,160,161,180,181,200,201,220,221,240,241,260,261,280,281,300,301,320,321,340,341,360,361,380,381 };
+    const int anchorIds[] = { 37 };
     const int handleIds[] = { 36,37,38,39,58,59,78,79,98,99,118,119,138,139,158,159,178,179,198,199,218,219,238,239,258,259,278,279,298,299,318,319,338,339,358,359,378,379,398,399 };
 
     deform::AsRigidAsPossibleDeformation arap(&mesh);
@@ -41,11 +41,11 @@ int main(int argc, char **argv) {
 
     for (int i = 0; i < nids; ++i) {
         //deform::Mesh::VertexHandle vh = mesh.vertex_handle(handleIds[i]);
-        deform::Mesh::VertexHandle vh = mesh.vertex_handle(0);
+        deform::Mesh::VertexHandle vh = mesh.vertex_handle(32);
         arap.setConstraint(vh, mesh.point(vh) + deform::Mesh::Point(0, 0, 1.0));
     }
 
-    arap.deform(3);   
+    arap.deform(10);   
 
     std::cout << "ok" << std::endl;
 
